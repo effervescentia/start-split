da6dbe743fa50db053eecf1d271a9430184f69e6
Ben Teichman <ben.l.teichman@gmail.com>
2017-06-08 02:46:39 -0400

    feat(sourcemap): fix source map roots based on relocation

--- src/index.js
+++ src/index.js
@@ -1,16 +1,39 @@
-import { resolve } from 'path';
+import path from 'path';
 import Start from 'start';
 import inputConnector from 'start-input-connector';
 import concurrent from 'start-concurrent';
+import { fromSource as getSourceMapFromSource, removeComments } from 'convert-source-map';
+
+const extendPath = (filepath, depth) => {
+  return path.join('..'.repeat(depth), filepath);
+};
 
 const split = (input, dirs, add) => {
   return Object.keys(dirs)
     .reduce((tasks, dir) => {
-      const srcDir = resolve(process.cwd(), dir);
+      const depth = dir.split(path.sep).length;
+      const srcDir = path.resolve(process.cwd(), dir);
       const filter = (file) => file.path.startsWith(srcDir);
 
       add(tasks, [
-        inputConnector(input.filter(filter)),
+        inputConnector(input.filter(filter)
+          .map((file) => {
+            if (file.map) {
+              // eslint-disable-next-line immutable/no-mutation
+              file.map.sourceRoot = extendPath(file.map.sourceRoot, depth);
+            } else {
+              const sourceMap = getSourceMapFromSource(file.data, true);
+
+              if (sourceMap) {
+                // eslint-disable-next-line max-len
+                sourceMap.setProperty('sourceRoot', extendPath(sourceMap.getProperty('sourceRoot'), depth));
+                // eslint-disable-next-line immutable/no-mutation
+                file.data = `${removeComments(file.data)}\n${sourceMap.toComment()}`;
+              }
+            }
+
+            return file;
+          })),
         ...dirs[dir]()
       ]);
 
@@ -22,13 +45,13 @@ const splitCon = (input, dirs) => function splitConcurrent(log, reporter) {
   const start = Start(reporter);
 
   return start(concurrent(
-    ...split(input, dirs, (tasks, subtasks) => tasks.push(() => start(...subtasks)))
+    ...split(input, dirs, (tasks, subtasks) => tasks.push(() => start(...subtasks)), log)
   ));
 };
 
 const splitSeq = (input, dirs) => function splitSequential(log, reporter) {
   return Start(reporter)(
-    ...split(input, dirs, (tasks, subtasks) => tasks.push(...subtasks))
+    ...split(input, dirs, (tasks, subtasks) => tasks.push(...subtasks), log)
   );
 };
 
